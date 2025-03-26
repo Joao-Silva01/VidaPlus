e59d386eaffe189760bc54ec6b3df5737dbaa74d
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2025-03-26 10:43:35 -0300

    feat: Create project

---
+++ src/test/java/com/RobDev/VidaPlus/VidaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RobDev.VidaPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
